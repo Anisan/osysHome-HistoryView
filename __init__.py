2b42847202645295744798782fb4ccbd66823ea3
Eraser <eraser1981@gmail.com>
2025-02-19 19:34:43 +0300

    history graph

--- __init__.py
+++ __init__.py
@@ -20,20 +20,14 @@ class HistoryView(BasePlugin):
         op = request.args.get("op",None)
         object_id = int(request.args.get("object",0))
         name = request.args.get("name",None)
-        page = int(request.args.get("page", 0))
         if op == 'delete':
             id = request.args.get("id",None)
             with session_scope() as session:
                 session.query(History).filter(History.id==id).delete()
                 session.commit()
-                return redirect(f'{self.name}?object={object_id}&name={name}&page={page}')
+                return redirect(f'{self.name}?object={object_id}&name={name}')
 
-        per_page = 30
         obj = Object.query.where(Object.id == object_id).one_or_none()
-        value = Value.query.where(Value.name == name, Value.object_id == object_id).one_or_none()
-        history = []
-        if value:
-            history = History.query.where(History.value_id == value.id).order_by(desc(History.added)).paginate(page=page, per_page=per_page, error_out=False)
-
-        return render_template('history.html', object=obj, name=name, data=history.items, total=history.total, page=page, pages=history.pages)
+        
+        return render_template('history.html', object=obj, name=name)
 
